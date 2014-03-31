e77393c004769b67131c6d4e690652241a9856e3
Schmoller <steven.schmoll@gmail.com>
2014-03-31 14:22:43 +1100

    Initial commit

---
+++ src/au/com/addstar/unscramble/Unscramble.java
@@ -0,0 +1,17 @@
+package au.com.addstar.unscramble;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Unscramble extends Plugin
+{
+	@Override
+	public void onEnable()
+	{
+	}
+	
+	@Override
+	public void onDisable()
+	{
+	}
+	
+}
